b2cee7c83c1cc9876fd5030c804f9721e4980587
Mali Gautam Hastimal <48179438+gautammali@users.noreply.github.com>
2021-06-05 15:01:34 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript with Reactjs</h1>
+      <p>
+        is very good learning session by ShapeAI in this explanation was too
+        good i was thing React is hard but it's easy
+      </p>
+    </div>
+  );
+}
+
+export default Note;
